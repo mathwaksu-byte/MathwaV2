88df383344a72d26946b0c4c2024e33b4e33e77b
mathwaksu-byte <mathwasku@gmail.com>
2025-11-18 02:31:20 +0530

    clientv

--- client/functions/[[path]].ts
+++ client/functions/[[path]].ts
@@ -1,7 +1,8 @@
 import { createPagesFunctionHandler } from "@remix-run/cloudflare-pages";
 
 export const onRequest = async (context: any) => {
-  const build = (await import("server-build")) as any;
+  const p = "../build/index.js";
+  const build = (await import(p)) as any;
   const handle = createPagesFunctionHandler({
     build,
     mode: process.env.NODE_ENV,
